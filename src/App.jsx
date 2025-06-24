bb039e8e3fa38e9b643c10fed32efebebded04d6
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-25 00:51:27 +0530

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-500 text-white text-3xl'>
+      Ai-code-reviewer
+    </div>
+  )
+}
+
+export default App
+    
