20f6fb86251581cf4a159bc0e29d70a176d4174b
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-25 01:44:00 +0530

    Adding react-editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "prismjs/themes/prism-tomorrow.css";
 import prism from "prismjs";
+import Editor from 'react-simple-code-editor'
 
 const App = () => {
+  const [code, setcode] = useState(`function sum(){
+    return a+b
+}`)
+
   useEffect(() => {
     prism.highlightAll();
   });
@@ -12,11 +17,20 @@ const App = () => {
       <main className="h-[98%] w-full p-[1.5rem] flex gap-[1rem] items-center justify-center">
         <div className="left relative h-full basis-1/2 bg-black rounded-[0.7rem]">
           <div className="code  ">
-            <pre className="">
-              <code className="language-javascript">
-                {}
-              </code>
-            </pre>
+            
+          <Editor
+              value={code}
+              onValueChange={code => setcode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript)}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                color: 'white',
+                overflow: 'auto'
+              }}
+            />
+            
           </div>
           <div className="review absolute bottom-[1rem] right-[1rem] bg-[rgb(219,219,255)] text-black px-[2rem] py-[0.5rem] rounded-[0.7rem] cursor-pointer font-bold select-none hover:scale-102">
             Review
