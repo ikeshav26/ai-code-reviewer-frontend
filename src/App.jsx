90429eb6bf4d0a6901d18c07c8aea0003d4df499
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-25 01:37:12 +0530

    Frontend built

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
 
 const App = () => {
-  return (
-    <div className='bg-red-500 text-white text-3xl'>
-      Ai-code-reviewer
-    </div>
-  )
-}
+  useEffect(() => {
+    prism.highlightAll();
+  });
 
-export default App
-    
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      <main className="h-[98%] w-full p-[1.5rem] flex gap-[1rem] items-center justify-center">
+        <div className="left relative h-full basis-1/2 bg-black rounded-[0.7rem]">
+          <div className="code  ">
+            <pre className="">
+              <code className="language-javascript">
+                {}
+              </code>
+            </pre>
+          </div>
+          <div className="review absolute bottom-[1rem] right-[1rem] bg-[rgb(219,219,255)] text-black px-[2rem] py-[0.5rem] rounded-[0.7rem] cursor-pointer font-bold select-none hover:scale-102">
+            Review
+          </div>
+        </div>
+        <div className="right h-full basis-1/2 bg-zinc-800 rounded-[0.7rem]"></div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
